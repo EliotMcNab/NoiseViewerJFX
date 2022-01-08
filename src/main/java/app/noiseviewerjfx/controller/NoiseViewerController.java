c4c8b453d676604c54a85b6a9e60d0f1354f2570
eliot <eliot.mcnab@laposte.net>
2022-01-08 19:04:14 +0100

    - Replaced fontAwesomeFX with ikonLi - started work on the terrain layer tab - made lots of progress in terms of styling

--- src/main/java/app/noiseviewerjfx/controller/NoiseViewerController.java
+++ src/main/java/app/noiseviewerjfx/controller/NoiseViewerController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.util.StringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,6 +29,10 @@ public class NoiseViewerController implements Initializable {
     private Spinner<Integer> HEIGHT_SPINNER;
     private final SpinnerValueFactory<Integer> heightSpinnerVF =
             new SpinnerValueFactory.IntegerSpinnerValueFactory(100, 1000, 1);
+    @FXML
+    private Spinner<Integer> OPACITY_SPINNER;
+    private final SpinnerValueFactory<Integer> opacitySpinnerVF =
+            new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 0);
 
     /*@FXML
     private CheckBox MASK_CHECKBOX;
@@ -50,14 +55,35 @@ public class NoiseViewerController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        opacitySpinnerVF.setConverter(new StringConverter<Integer>() {
+            @Override
+            public String toString(Integer integer) {
+                return integer.toString() + "%";
+            }
+
+            @Override
+            public Integer fromString(String valueString) {
+                String valueWithoutUnits = valueString.replaceAll("%", "").trim();
+                if (valueWithoutUnits.isEmpty()) return 0;
+
+                try {
+                    return Integer.parseInt(valueWithoutUnits);
+                } catch (NumberFormatException e) {
+                    return 0;
+                }
+            }
+        });
+
         OCTAVE_SPINNER.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
         PERSISTENCE_SPINNER.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
         WIDTH_SPINNER.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
         HEIGHT_SPINNER.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
+        OPACITY_SPINNER.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
 
         OCTAVE_SPINNER.setValueFactory(octaveSpinnerVF);
         PERSISTENCE_SPINNER.setValueFactory(persistenceSpinnerVF);
         WIDTH_SPINNER.setValueFactory(widthSpinnerVF);
         HEIGHT_SPINNER.setValueFactory(heightSpinnerVF);
+        OPACITY_SPINNER.setValueFactory(opacitySpinnerVF);
     }
 }
